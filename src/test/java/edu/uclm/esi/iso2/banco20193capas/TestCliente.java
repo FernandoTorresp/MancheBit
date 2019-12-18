a58d9ce2fbd2653aef0618b7d10bae22ed0c8f7a
Víctor <Vctor@LAPTOP-CIG8I2A0>
2019-12-19 00:24:24 +0100

    Creación de 4 @Test en clase TestCliente.java
    Estrategia de cobertura pairwise + @Test extra de gettes tribiales

--- src/test/java/edu/uclm/esi/iso2/banco20193capas/TestCliente.java
+++ src/test/java/edu/uclm/esi/iso2/banco20193capas/TestCliente.java
@@ -1,6 +1,7 @@
 package edu.uclm.esi.iso2.banco20193capas;
 
 import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,11 +20,20 @@ import edu.uclm.esi.iso2.banco20193capas.model.Tarjeta;
 import edu.uclm.esi.iso2.banco20193capas.model.TarjetaCredito;
 import junit.framework.TestCase;
 
+import java.math.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestCliente extends TestCase {
 	
 	private Cliente pepe;
+
+	private Long id1=-9223372036854775808l;
+	private Long id2=-90000l;
+	private Long id3=0l;
+	private Long id4=123456l;
+	private Long id5=9223372036854775807l;
+		
 	
 	@Before
 	public void setUp() {
@@ -36,6 +46,152 @@ public class TestCliente extends TestCase {
 		
 		this.pepe = new Cliente("12345X", "Pepe", "Pérez"); this.pepe.insert();
 	}
+
+	
+	@Test
+	public void testSetClientePairWise1() {
+		try {
+			
+			pepe.setId(id1);
+			pepe.setNif("01111111A");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id2);
+			pepe.setNif("01111111A");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id3);
+			pepe.setNif("01111111A");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id4);
+			pepe.setNif("01111111A");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id5);
+			pepe.setNif("01111111A");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}		
 		
+	}
+	
+	@Test
+	public void testSetClientePairWise2() {
+		try {
+			
+			pepe.setId(id1);
+			pepe.setNombre("José");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id2);
+			pepe.setNombre("José");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id3);
+			pepe.setNombre("José");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id4);
+			pepe.setNombre("José");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id5);
+			pepe.setNombre("José");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}		
+		
+	}
+	
+	@Test
+	public void testSetClientePairWise3() {
+		try {
+			
+			pepe.setId(id1);
+			pepe.setApellidos("Pérez");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id2);
+			pepe.setApellidos("Pérez");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id3);
+			pepe.setApellidos("Pérez");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id4);
+			pepe.setApellidos("Pérez");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+		try {
+			
+			pepe.setId(id5);
+			pepe.setApellidos("Pérez");
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}		
+		
+	}
+	
+	@Test
+	public void getNombreApellidos() {
+		try {
+			pepe.getNombre();
+			pepe.getApellidos();
+			
+		}catch(Exception e) {
+			fail("No se esperaba excepción");
+		}
+	}
 
 }
